7a0bdade11447839fa215a64210d20a93c37ca66
Hanuman <50351347+hanubc7743@users.noreply.github.com>
2019-07-12 18:26:31 +0530

    Hanuman--changes for identity and loan states

--- clients/src/main/kotlin/com/ledgerblocks/poc/server/MainController.kt
+++ clients/src/main/kotlin/com/ledgerblocks/poc/server/MainController.kt
@@ -74,7 +74,7 @@ class MainController(rpc: NodeRPCConnection) {
         return try {
 
             val accountId=proxy.startTrackedFlow(::IdentityStateFlow, identityname,imei,type).returnValue.get()
-            ResponseEntity.status(HttpStatus.CREATED).body("UUID is ${accountId} \n")
+            ResponseEntity.status(HttpStatus.CREATED).body("UUID:${accountId} \n")
 
         } catch (ex: Throwable) {
             logger.error(ex.message, ex)
@@ -118,7 +118,7 @@ class MainController(rpc: NodeRPCConnection) {
         return try {
 
             val accountId=proxy.startTrackedFlow(::LoanStateFlow, UUID.fromString(uuid),loanAmount,loanPeriod,loanPurpose,interestRate,emi).returnValue.get()
-            ResponseEntity.status(HttpStatus.CREATED).body("UUID is ${accountId} \n")
+            ResponseEntity.status(HttpStatus.CREATED).body("decision:${loan} \n")
 
         } catch (ex: Throwable) {
             logger.error(ex.message, ex)
