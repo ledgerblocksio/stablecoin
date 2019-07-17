8e4378d5c9e028d9f8dcb30e77c2fdacaa6e521c
Hanuman <50351347+hanubc7743@users.noreply.github.com>
2019-07-17 19:02:22 +0530

    committed by hanu--LoanStateFlow

--- workflows-kotlin/src/main/kotlin/com/ledgerblocks/poc/flow/LoanStateFlow.kt
+++ workflows-kotlin/src/main/kotlin/com/ledgerblocks/poc/flow/LoanStateFlow.kt
@@ -40,7 +40,7 @@ class LoanStateFlow(private val uuid: UUID,private val loanAmount: Int,private v
        val accountInfo=accountService.accountInfo(uuid)
        val lAmount = subFlow(IssueTokenToAccountIdFlow(uuid,loanAmount))
        if (count==0)
-           loanDecision="Aprroved "+ lAmount
+           loanDecision="Aprroved"+"loanAmount:"+ lAmount
 
         else
            loanDecision="Rejected"
