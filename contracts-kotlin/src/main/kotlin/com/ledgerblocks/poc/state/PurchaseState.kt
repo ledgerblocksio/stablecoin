5190c3e202aba46514db0342c492563fea6a5ddf
Hanuman <50351347+hanubc7743@users.noreply.github.com>
2019-07-17 18:59:07 +0530

    committed by hanu--PurchaseState

---
+++ contracts-kotlin/src/main/kotlin/com/ledgerblocks/poc/state/PurchaseState.kt
@@ -0,0 +1,22 @@
+package com.ledgerblocks.poc.state
+
+import com.ledgerblocks.poc.contract.LoanContract
+import net.corda.core.contracts.BelongsToContract
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
+import java.util.*
+
+@BelongsToContract( LoanContract::class)
+data class PurchaseState(
+        val bUuid: UUID,
+        val mUuid: UUID,
+        val purchase: String,
+        val host: Party,
+        override val linearId: UniqueIdentifier = UniqueIdentifier()
+) :
+        LinearState {
+
+    override val participants: List<AbstractParty> get() = listOf(host)
+}
