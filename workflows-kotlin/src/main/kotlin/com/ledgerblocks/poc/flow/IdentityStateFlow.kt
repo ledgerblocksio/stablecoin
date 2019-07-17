c5ff209a2854cc0d0e81850eb4df176855749507
Hanuman <50351347+hanubc7743@users.noreply.github.com>
2019-07-17 19:02:46 +0530

    committed by hanu--IdentityStateFlow

--- workflows-kotlin/src/main/kotlin/com/ledgerblocks/poc/flow/IdentityStateFlow.kt
+++ workflows-kotlin/src/main/kotlin/com/ledgerblocks/poc/flow/IdentityStateFlow.kt
@@ -4,11 +4,9 @@ import co.paralleluniverse.fibers.Suspendable
 import com.ledgerblocks.poc.contract.IdentityContract
 import com.ledgerblocks.poc.state.IdentityState
 
-import net.corda.accounts.flows.RequestKeyForAccountFlow
 import net.corda.accounts.service.KeyManagementBackedAccountService
-import net.corda.accounts.states.AccountInfo
-import net.corda.core.contracts.StateAndRef
 import net.corda.core.flows.*
+import net.corda.core.identity.CordaX500Name
 
 
 import net.corda.core.transactions.SignedTransaction
@@ -18,29 +16,37 @@ import java.util.*
 
 @InitiatingFlow
 @StartableByRPC
-class IdentityStateFlow(private val identityname: String,private val imei: String,private val type: String): FlowLogic<UUID>(){
+class IdentityStateFlow(private val name: String, private val imei: String, private val type: String): FlowLogic<UUID>(){
 
     @Suspendable
     override fun call(): UUID {
         val notary = serviceHub.networkMapCache.notaryIdentities.first()
         val  newImei:String
+        val  newParty : String
         if(type.equals('m')) {
              newImei=type+imei
+            newParty= "O=PartyA,L=London,C=GB"
         }
         else if (type.equals('b'))
         {
             newImei=type+imei
+            newParty= "O=PartyB,L=New York,C=US"
         }
           else
         {
             newImei=imei
+           newParty= "O=PartyA,L=London,C=GB"
         }
-            val id = type+identityname + newImei
+            val id = type+name + newImei
             val accountService = serviceHub.cordaService(KeyManagementBackedAccountService::class.java)
             val newAccountCreation = accountService.createAccount(id)
             val storedAccountInfo = accountService.accountInfo(id)
-            //val freshkeyNewAccount = subFlow(RequestKeyForAccountFlow(storedAccountInfo!!.state.data))
-            val identityState = IdentityState(identityname, imei, storedAccountInfo!!.state.data.accountId, storedAccountInfo.state.data.accountHost)
+       val x500Name = CordaX500Name.parse(newParty)
+        val party= serviceHub.networkMapCache.getPeerByLegalName(x500Name)!!
+
+            accountService.shareAccountInfoWithParty(storedAccountInfo!!.state.data.accountId, party)
+        val accounts = accountService.allAccounts()
+            val identityState = IdentityState(name, imei, storedAccountInfo!!.state.data.accountId, storedAccountInfo.state.data.accountHost)
             val transactionBuilder = TransactionBuilder(notary)
                 .addOutputState(identityState)
                 .addCommand(IdentityContract.OPEN,serviceHub.myInfo.legalIdentities.first().owningKey)
