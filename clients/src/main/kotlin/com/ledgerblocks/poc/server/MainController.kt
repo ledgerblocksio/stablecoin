46d7a8fcb6e25986e2fbd8211985decd3897a11f
Hanuman <50351347+hanubc7743@users.noreply.github.com>
2019-07-21 18:10:00 +0530

    committed by hanu on 21/july/2019

--- clients/src/main/kotlin/com/ledgerblocks/poc/server/MainController.kt
+++ clients/src/main/kotlin/com/ledgerblocks/poc/server/MainController.kt
@@ -1,6 +1,5 @@
 package com.ledgerblocks.poc.server
 
-//import com.ledgerblocks.poc.dto.Identity
 import com.ledgerblocks.poc.flow.GoodsPurchaseFlow
 import com.ledgerblocks.poc.flow.IdentityStateFlow
 import com.ledgerblocks.poc.flow.LoanStateFlow
@@ -8,18 +7,14 @@ import com.ledgerblocks.poc.state.IdentityState
 import com.ledgerblocks.poc.state.LoanState
 import com.ledgerblocks.poc.state.TokenState
 import net.corda.core.contracts.StateAndRef
-import net.corda.core.identity.CordaX500Name
-
 import net.corda.core.messaging.startTrackedFlow
 import net.corda.core.messaging.vaultQueryBy
-import net.corda.core.utilities.getOrThrow
-
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.net.URI
+
 import java.util.*
 
 import javax.servlet.http.HttpServletRequest
@@ -51,8 +46,8 @@ class MainController(rpc: NodeRPCConnection) {
     /**
      * Displays all amounts states that exist in the node's vault.
      */
-    @GetMapping(value = ["amounts"], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun getAmounts(): ResponseEntity<List<StateAndRef<IdentityState>>> {
+    @GetMapping(value = ["identityStates"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getidentityStates(): ResponseEntity<List<StateAndRef<IdentityState>>> {
         return ResponseEntity.ok(proxy.vaultQueryBy<IdentityState>().states)
     }
 
@@ -60,12 +55,10 @@ class MainController(rpc: NodeRPCConnection) {
      * createIdentity
      */
     @PostMapping(value = ["identity"], produces = [MediaType.APPLICATION_JSON_VALUE])
-
     fun createIdentity(request: HttpServletRequest): ResponseEntity<Any?> {
         val name = request.getParameter("name")
         val imei = request.getParameter("imei")
         val type = request.getParameter("type")
-        //  val otherParty = request.getParameter("otherParty")
         if (name == null) {
             return ResponseEntity.badRequest().body("Query parameter 'name' must not be null.\n")
         }
@@ -75,9 +68,7 @@ class MainController(rpc: NodeRPCConnection) {
         if (type == null) {
             return ResponseEntity.badRequest().body("Query parameter 'type' must not be null.\n")
         }
-       
         return try {
-           
             val accountId = proxy.startTrackedFlow(::IdentityStateFlow, name, imei, type).returnValue.get()
             ResponseEntity.status(HttpStatus.CREATED).body("uuid:${accountId}")
         } catch (ex: Throwable) {
@@ -93,8 +84,6 @@ class MainController(rpc: NodeRPCConnection) {
 
     @PostMapping(value = ["loan"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun createLoan(request: HttpServletRequest): ResponseEntity<String> {
-
-
         val uuid = request.getParameter("uuid")
         val uuid1 = UUID.fromString(uuid)
         val loanAmount = request.getParameter("loanAmount").toInt()
@@ -134,32 +123,37 @@ class MainController(rpc: NodeRPCConnection) {
     /**
      * createPurchase
      */
-
     @PostMapping(value = ["purchase"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun createPurchase(request: HttpServletRequest): ResponseEntity<String> {
-        val bUuid = request.getParameter("bUuid")
+        val bUuid = request.getParameter("bUUID")
         val bUuid1 = UUID.fromString(bUuid)
-        val mUuid = request.getParameter("mUuid")
+        val mUuid = request.getParameter("mUUID")
         val mUuid1 = UUID.fromString(mUuid)
-        val purchaseAmt = request.getParameter("purchaseAmt").toInt()
+        val purchaseAmt = request.getParameter("purchaseAmount").toInt()
         val goodsDesc = request.getParameter("goodsDesc")
         if (bUuid == null) {
-            return ResponseEntity.badRequest().body("Query parameter 'bUuid' must not be null.\n")
+            return ResponseEntity.badRequest().body("Query parameter 'bUUID' must not be null.\n")
         }
         if (mUuid == null) {
-            return ResponseEntity.badRequest().body("Query parameter 'mUuid' must not be null.\n")
+            return ResponseEntity.badRequest().body("Query parameter 'mUUID' must not be null.\n")
         }
         if (purchaseAmt <= 0) {
-            return ResponseEntity.badRequest().body("Query parameter ' purchaseAmt' must be non-negative..\n")
+            return ResponseEntity.badRequest().body("Query parameter ' purchaseAmount' must be non-negative..\n")
         }
         if (goodsDesc == null) {
             return ResponseEntity.badRequest().body("Query parameter 'goodsDesc' must not be null.\n")
         }
         return try {
             val purchase = proxy.startTrackedFlow(::GoodsPurchaseFlow, bUuid1, mUuid1, purchaseAmt, goodsDesc).returnValue.get()
-          
-            ResponseEntity.status(HttpStatus.CREATED).body("${purchase}")
-            
+            val bUuidTokenStateinfo=proxy.vaultQueryBy<TokenState>().states.filter { it.state.data.accountId.equals(bUuid1)}
+            bUuidTokenStateinfo.forEach{tokenState:StateAndRef<TokenState> ->
+                val stateAmount = tokenState.state.data.amount
+                println("amount=$stateAmount")
+            }
+            val borrowerTokenBal= bUuidTokenStateinfo.get(bUuidTokenStateinfo.size-1).state.data.amount
+            val bUuidLoanStateinfo=proxy.vaultQueryBy<LoanState>().states.filter { it.state.data.uuid.equals(bUuid1)}
+            val bInitialLoanAmount= bUuidLoanStateinfo.get(0).state.data.loanAmount
+            ResponseEntity.status(HttpStatus.CREATED).body("purchase:${purchase}+loanAmount:${bInitialLoanAmount}+avaBlance:${borrowerTokenBal}")
         } catch (ex: Throwable) {
             logger.error(ex.message, ex)
             ResponseEntity.badRequest().body(ex.message!!)
