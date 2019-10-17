1986a24df924c323f2696cbc84935333199cc920
Hanuman <50351347+hanubc7743@users.noreply.github.com>
2019-10-17 07:06:29 +0100

    Update DeductLoanAmountFlow.kt

--- workflows-kotlin/src/main/kotlin/com/ledgerblocks/poc/flow/DeductLoanAmountFlow.kt
+++ workflows-kotlin/src/main/kotlin/com/ledgerblocks/poc/flow/DeductLoanAmountFlow.kt
@@ -29,13 +29,13 @@ class DeductLoanAmountFlow(private val bUUID: UUID,private val amtToPay: Int): F
         val bUuidLoanStateinfo=serviceHub.vaultService.queryBy<LoanState>().states.filter {it.state.data.uuid.equals(bUUID)  }
 
         println("bUuidLoanStateinfo=$bUuidLoanStateinfo")
-        val bInitialLoanAmount= bUuidLoanStateinfo.get(0).state.data.loanAmount
-        val loanPeriod=bUuidLoanStateinfo.get(0).state.data.loanPeriod
-        val loanPurpose=bUuidLoanStateinfo.get(0).state.data.loanPurpose
-        val loanDecision=bUuidLoanStateinfo.get(0).state.data.loanDecision
-        val interestRate=bUuidLoanStateinfo.get(0).state.data.interestRate
-        val emi=bUuidLoanStateinfo.get(0).state.data.emi
-        val loanState = LoanState(bUUID,amtToPay-bInitialLoanAmount, loanPeriod, loanPurpose,loanDecision,interestRate,emi, bAccountInfo!!.state.data.accountHost)
+        val bInitialLoanAmount= bUuidLoanStateinfo.get(bUuidLoanStateinfo.size-1).state.data.loanAmount
+        val loanPeriod=bUuidLoanStateinfo.get(bUuidLoanStateinfo.size-1).state.data.loanPeriod
+        val loanPurpose=bUuidLoanStateinfo.get(bUuidLoanStateinfo.size-1).state.data.loanPurpose
+        val loanDecision=bUuidLoanStateinfo.get(bUuidLoanStateinfo.size-1).state.data.loanDecision
+        val interestRate=bUuidLoanStateinfo.get(bUuidLoanStateinfo.size-1).state.data.interestRate
+        val emi=bUuidLoanStateinfo.get(bUuidLoanStateinfo.size-1).state.data.emi
+        val loanState = LoanState(bUUID,bInitialLoanAmount-amtToPay, loanPeriod, loanPurpose,loanDecision,interestRate,emi, bAccountInfo!!.state.data.accountHost)
         val transactionBuilder = TransactionBuilder(notary)
                 .addOutputState(loanState)
                 .addCommand(LoanContract.Commands.Loan(),serviceHub.myInfo.legalIdentities.first().owningKey)
@@ -43,7 +43,25 @@ class DeductLoanAmountFlow(private val bUUID: UUID,private val amtToPay: Int): F
         transactionBuilder.verify(serviceHub)
         println("test=$loanDecision")
 
-        return subFlow(FinalityFlow(signedTransaction, emptyList()))
+        val accountSession = initiateFlow(bAccountInfo!!.state.data.accountHost)
+        val sessions = if (!serviceHub.myInfo.isLegalIdentity(bAccountInfo.state.data.accountHost))
+            Collections.singletonList(accountSession)
+        else
+            Collections.emptyList()
+
+        return subFlow(FinalityFlow(signedTransaction,sessions)).also {
+
+            val broadcastToParties =
+                    serviceHub.networkMapCache.allNodes.map { node -> node.legalIdentities.first() }
+                            .minus(serviceHub.networkMapCache.notaryIdentities)
+            //.minus(mParty)
+            //  .minus(bParty)
+            subFlow(
+                    BroadcastTransactionFlow(
+                            it, broadcastToParties
+                    )
+            )
+        }
 
     }
 }
