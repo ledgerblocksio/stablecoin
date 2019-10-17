5b196d495f05d6c4750f9f20df9ddc76f2d5f621
Hanuman <50351347+hanubc7743@users.noreply.github.com>
2019-10-17 07:13:23 +0100

    Update PayLoanDirectWithMerchantFlow.kt

--- workflows-kotlin/src/main/kotlin/com/ledgerblocks/poc/flow/PayLoanDirectWithMerchantFlow.kt
+++ workflows-kotlin/src/main/kotlin/com/ledgerblocks/poc/flow/PayLoanDirectWithMerchantFlow.kt
@@ -2,11 +2,13 @@ package com.ledgerblocks.poc.flow
 
 import co.paralleluniverse.fibers.Suspendable
 
-import com.ledgerblocks.poc.contract.LoanContract
 
+import com.ledgerblocks.poc.contract.TokenContract
 import com.ledgerblocks.poc.state.PayLoanState
 
+
 import com.ledgerblocks.poc.state.TokenState
+import net.corda.accounts.flows.RequestKeyForAccountFlow
 import net.corda.accounts.service.KeyManagementBackedAccountService
 
 import net.corda.core.flows.*
@@ -15,53 +17,129 @@ import net.corda.core.node.services.queryBy
 
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
-import java.io.FileInputStream
+
+import java.time.LocalDate
 
 import java.util.*
 
 @InitiatingFlow
 @StartableByRPC//private val name: String
-class PayLoanDirectThroughMerchantFlow(private val bUUID: UUID, private val mUUID: UUID, private val lbUUID: UUID, private val amtToPay1: Int): FlowLogic<String>(){
+class PayLoanDirectThroughMerchantFlow(private val bUUID: UUID, private val mUUID: UUID, private val lbUUID: UUID, private val amtToPay1: Int): FlowLogic<SignedTransaction>(){
     @Suspendable
-    override fun call(): String {
+    override fun call(): SignedTransaction {
         val notary = serviceHub.networkMapCache.notaryIdentities.first()
         val payment:String
 
         val accountService = serviceHub.cordaService(KeyManagementBackedAccountService::class.java)
 
-      //  subFlow(DeductLoanAmountFlow(bUUID,amtToPay1))
-        requestDeductLoan(bUUID,amtToPay1)
-        subFlow(AddTokensToMerchantFlow(mUUID,amtToPay1))
-        val mTokenStateinfo=serviceHub.vaultService.queryBy<TokenState>().states.filter {it.state.data.toAccountId.equals(mUUID)}
-        val mTokenBal = mTokenStateinfo.get(mTokenStateinfo.size-1).state.data.amount
+         subFlow(DeductLoanAmountFlow(bUUID,amtToPay1))
+
+      //  requestDeductLoan(bUUID,amtToPay1)
+      //  subFlow(AddTokensToMerchantFlow(bUUID,mUUID,amtToPay1))
+        val mTokenStateinfo=serviceHub.vaultService.queryBy<TokenState>().states.filter { it.state.data.toAccountId!!.equals(mUUID)}
+        val mTokenBal = mTokenStateinfo.get(mTokenStateinfo.size-1).state.data.tokenBalance
         val lbAccountInfo = accountService.accountInfo(lbUUID)
 
         println("lbAccountInfo=$lbAccountInfo")
+        val mAccountInfo=accountService.accountInfo(mUUID)
+        val mParty = mAccountInfo!!.state.data.accountHost
+        val freshkeyToAccount = subFlow(RequestKeyForAccountFlow(mAccountInfo!!.state.data))
+        //  val bInitialLoanAmount= bUuidLoanStateinfo.get(0).state.data.loanAmount
+        //val exchange:String
 
-        if(!(mTokenBal.equals(0))) {
+        if(!(amtToPay1.equals(0))) {
+
             payment = "success"
+
+            val paymentState= PayLoanState(mUUID,lbUUID,payment, mAccountInfo!!.state.data.accountHost)
+            val updatedAmount: Int
+            //var resultMoveTokenInfo:SignedTransaction ? = null
+
+            updatedAmount = mTokenBal - amtToPay1
+
+
+            val tokenState = TokenState(updatedAmount, amtToPay1, mUUID, lbUUID, "payloanMerchant", LocalDate.now().toString(), freshkeyToAccount.owningKey, mAccountInfo!!.state.data.accountHost, listOf(mAccountInfo!!.state.data.accountHost))
+
+            // val tokenState = TokenState(mParty,mParty,updatedAmount,uuid,uuid,exchange,freshkeyToAccount.owningKey, mAccountInfo!!.state.data.accountHost, listOf(mAccountInfo!!.state.data.accountHost))
+
+            val transactionBuilder = TransactionBuilder(notary)
+                    //.addInputState(resultMoveTokenInfo!!.coreTransaction.outRefsOfType<TokenState>().single())
+                    .addOutputState(tokenState)
+                    .addOutputState(paymentState)
+                    .addCommand(TokenContract.Commands.Exchange(), serviceHub.myInfo.legalIdentities.first().owningKey)
+            val signedTransaction = serviceHub.signInitialTransaction(transactionBuilder)
+            transactionBuilder.verify(serviceHub)
+            val accountSession = initiateFlow(mAccountInfo!!.state.data.accountHost)
+            val sessions = if (!serviceHub.myInfo.isLegalIdentity(mAccountInfo.state.data.accountHost))
+                Collections.singletonList(accountSession)
+            else
+                Collections.emptyList()
+            return subFlow(FinalityFlow(signedTransaction, sessions)).also {
+
+                val broadcastToParties =
+                        serviceHub.networkMapCache.allNodes.map { node -> node.legalIdentities.first() }
+                                .minus(serviceHub.networkMapCache.notaryIdentities)
+                //.minus(mParty)
+                //  .minus(bParty)
+                subFlow(
+                        BroadcastTransactionFlow(
+                                it, broadcastToParties
+                        )
+                )
+            }
+
         }
+
+
         else {
             payment = "failure"
+
+            val paymentState = PayLoanState(mUUID, lbUUID, payment, mAccountInfo!!.state.data.accountHost)
+            val updatedAmount: Int
+            //var resultMoveTokenInfo:SignedTransaction ? = null
+
+            updatedAmount = mTokenBal
+
+
+            val tokenState = TokenState(updatedAmount, amtToPay1, mUUID, lbUUID, "payloanMerchant", LocalDate.now().toString(), freshkeyToAccount.owningKey, mAccountInfo!!.state.data.accountHost, listOf(mAccountInfo!!.state.data.accountHost))
+
+            // val tokenState = TokenState(mParty,mParty,updatedAmount,uuid,uuid,exchange,freshkeyToAccount.owningKey, mAccountInfo!!.state.data.accountHost, listOf(mAccountInfo!!.state.data.accountHost))
+
+            val transactionBuilder = TransactionBuilder(notary)
+                    //.addInputState(resultMoveTokenInfo!!.coreTransaction.outRefsOfType<TokenState>().single())
+                    .addOutputState(tokenState)
+                    .addOutputState(paymentState)
+                    .addCommand(TokenContract.Commands.Exchange(), serviceHub.myInfo.legalIdentities.first().owningKey)
+            val signedTransaction = serviceHub.signInitialTransaction(transactionBuilder)
+            transactionBuilder.verify(serviceHub)
+            val accountSession = initiateFlow(mAccountInfo!!.state.data.accountHost)
+            val sessions = if (!serviceHub.myInfo.isLegalIdentity(mAccountInfo.state.data.accountHost))
+                Collections.singletonList(accountSession)
+            else
+                Collections.emptyList()
+            return subFlow(FinalityFlow(signedTransaction, sessions)).also {
+
+                val broadcastToParties =
+                        serviceHub.networkMapCache.allNodes.map { node -> node.legalIdentities.first() }
+                                .minus(serviceHub.networkMapCache.notaryIdentities)
+                //.minus(mParty)
+                //  .minus(bParty)
+                subFlow(
+                        BroadcastTransactionFlow(
+                                it, broadcastToParties
+                        )
+                )
+            }
         }
-        val mAccountInfo=accountService.accountInfo(mUUID)
-        val payLoanState= PayLoanState(mUUID,lbUUID,payment, mAccountInfo!!.state.data.accountHost)
-        val transactionBuilder = TransactionBuilder(notary)
-                .addInputState(mTokenStateinfo.get(mTokenStateinfo.size-1))
-                .addOutputState(payLoanState)
-                .addCommand(LoanContract.Commands.Pay(),serviceHub.myInfo.legalIdentities.first().owningKey)
-        val signedTransaction = serviceHub.signInitialTransaction(transactionBuilder)
-        transactionBuilder.verify(serviceHub)
-        val accountSession = initiateFlow(lbAccountInfo!!.state.data.accountHost)
-        val sessions = if (!serviceHub.myInfo.isLegalIdentity(lbAccountInfo.state.data.accountHost))
-            Collections.singletonList(accountSession)
-        else
-            Collections.emptyList()
-        return subFlow(FinalityFlow(signedTransaction, sessions)).coreTransaction.outRefsOfType<PayLoanState>().single().state.data.payment
+    }
 
     }
 
-}
+
+
+
+
+
 
 @InitiatedBy(PayLoanDirectThroughMerchantFlow::class)
 class PayLoanDirectThroughMerchantFlowResponderFlow(val otherPartySession: FlowSession): FlowLogic<SignedTransaction>(){
